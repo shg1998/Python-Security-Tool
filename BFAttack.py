d9a42bc36e2aab94da8c8664aae3805d7e25f675
shg1998 <moh.nezh1377@gmail.com>
2020-12-28 17:10:00 +0330

    whois and subDomain detector feature added

--- BFAttack.py
+++ BFAttack.py
@@ -1,8 +1,14 @@
 from os import system
 from itertools import product
 from string import ascii_lowercase
+from csv import reader
 import csv
 import time
+import whois
+
+# Author : Shg
+# in the next Release I will Store the information in DB
+# in this Release , Results have been shown in Console and and last of them stores in .csv file(need to become efficient!)
 
 
 def Listappender(Inputlist, num):
@@ -25,9 +31,7 @@ def GenerateWords(myList, digit):
     return urls
 
 
-if __name__ == "__main__":
-
-    print("Shg Attacker!!")
+def FindBruteForceAvailableWebsites():
     wordNumber = int(
         input('please enter number of word do you want for test : '))
     Domain = str(
@@ -47,3 +51,50 @@ if __name__ == "__main__":
             ActiveURLs.append(item)
 
     CsvWriter(ActiveURLs)
+
+
+def DictionaryCheckAvailabilityOfWebsites():
+    ActiveSubDomains = []
+    WhoisInformations = []
+    Domain = str(
+        input('and please enter TLD: '))
+    with open('Dictionary.csv', 'r') as read_obj:
+        csv_reader = reader(read_obj)
+        header = next(csv_reader)
+        if header != None:
+            print('SubDomains:')
+            for row in csv_reader:
+                row[0] = row[0]+Domain
+                res = system("ping -n 1 "+row[0])
+                if res == 0:
+                    ActiveSubDomains.append(row[0])
+                    print(f'____________________________{row[0]}________________________________')
+                    domain = whois.whois(row[0])
+                    print(domain)
+                    WhoisInformations.append(domain)
+        resu=int(input('Do You want to store this result in .csv File(negative number for No and positive number for Yes)'))
+        if resu >=0:
+            i=0
+            for var in WhoisInformations:
+                        with open('ValidUrlLogger.csv', 'w') as f:
+                            for key in var.keys():
+                                    f.write("%s,%s\n"%(key,var[key]))
+        elif resu<0:
+            exit()       
+      
+
+if __name__ == "__main__":
+
+    print("Welcome To Shg Attacker Tool!!")
+    print("Choose Your Tool!")
+    print("1) Find Available websites With Your own Words and TLD!")
+    print("2) Dictionary Check availability of Websites!")
+    print("3) Comming Soon!")
+    Choice = int(input())
+    if Choice == 1:
+        FindBruteForceAvailableWebsites()
+    elif Choice == 2:
+        DictionaryCheckAvailabilityOfWebsites()
+    elif Choice == 3:
+        print("See You soon!")
+        exit()
